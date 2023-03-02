c2607856bf1d81d14b4eb7dbf550870a436c0654
hritex <neon147369@gmail.com>
2023-03-02 17:26:54 +0530

    Added drag and drop function.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,98 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  fruits = [
+    {
+      "name" : "Apple",
+      "image" : "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Red_Apple.jpg/265px-Red_Apple.jpg",
+      "price" : 35
+    },
+    {
+      "name" : "Banana",
+      "image" : "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Bananas_white_background_DS.jpg/320px-Bananas_white_background_DS.jpg",
+      "price" : 12
+    },
+    {
+      "name" : "Grapes",
+      "image" : "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Table_grapes_on_white.jpg/320px-Table_grapes_on_white.jpg",
+      "weight": 0.1,
+      "price" : 45
+    },
+    {
+      "name" : "Pineapple",
+      "image" : "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Pineapple_and_cross_section.jpg/286px-Pineapple_and_cross_section.jpg",
+      "price" : 200
+    }
+  ];
+
+  colors = [
+    {
+      "color": "black",
+      "category": "hue",
+      "type": "primary",
+      "code": {
+        "rgba": [255,255,255,1],
+        "hex": "#000"
+      }
+    },
+    {
+      "color": "white",
+      "category": "value",
+      "code": {
+        "rgba": [0,0,0,1],
+        "hex": "#FFF"
+      }
+    },
+    {
+      "color": "red",
+      "category": "hue",
+      "type": "primary",
+      "code": {
+        "rgba": [255,0,0,1],
+        "hex": "#FF0"
+      }
+    },
+    {
+      "color": "blue",
+      "category": "hue",
+      "type": "primary",
+      "code": {
+        "rgba": [0,0,255,1],
+        "hex": "#00F"
+      }
+    },
+    {
+      "color": "yellow",
+      "category": "hue",
+      "type": "primary",
+      "code": {
+        "rgba": [255,255,0,1],
+        "hex": "#FF0"
+      }
+    },
+    {
+      "color": "green",
+      "category": "hue",
+      "type": "secondary",
+      "code": {
+        "rgba": [0,255,0,1],
+        "hex": "#0F0"
+      }
+    },
+  ];
+  
+
+  drop(event: CdkDragDrop<any>) {
+    console.log("Previous Index = " , event.previousIndex);
+    console.log("Current Index = " , event.currentIndex);
+    
+    moveItemInArray(this.fruits, event.previousIndex, event.currentIndex);
+  }
+
+  dropC(event: CdkDragDrop<any>) {
+    console.log("Previous Index = " , event.previousIndex);
+    console.log("Current Index = " , event.currentIndex);
+    
+    moveItemInArray(this.colors, event.previousIndex, event.currentIndex);
+  }
 }
