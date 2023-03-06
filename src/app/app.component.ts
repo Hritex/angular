0421eaf9d482be4eab6bf693996fd8ff1dc197bc
hritex <neon147369@gmail.com>
2023-03-06 13:18:47 +0530

    Assignment 3 Solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  showSecret = false;
+  // log: Array<number> = [];
+  log: any = [];
+
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    // this.log.push(this.log.length + 1);
+    this.log.push(new Date());
+  }
+
   fruits = [
     {
       "name" : "Apple",
