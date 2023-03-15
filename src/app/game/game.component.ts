a8e39deb81feb6e395e6eedaa213c02f61a11ed9
hritex <neon147369@gmail.com>
2023-03-15 10:50:50 +0530

    Assign Solu 1&2

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -6,26 +6,37 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./game.component.css']
 })
 export class GameComponent {
-  @Output() gameStarted = new EventEmitter<{count: number}>();
+  @Output() intervalFired = new EventEmitter<number>();
+  interval: any;
+  lastNumber = 0;
 
-  cnt = 0;
-  count: number | any;
-
-  startGame() {
-    this.cnt = this.cnt + 1;
-    // console.log('Count is ' + this.cnt);
+  onStartGame() {
+    this.interval = setInterval(() => {
+      this.intervalFired.emit(this.lastNumber + 1);
+      this.lastNumber++;
+    }, 1000)
   }
 
-  setIntervl(): any{
-    setInterval(() => this.startGame(), 1000);
-    this.gameStarted.emit({
-      count: this.cnt
-    });
-  }
+  // @Output() gameStarted = new EventEmitter<{count: number}>();
 
-  stopIntervl(){
-    clearInterval(this.setIntervl());
-  }
+  // cnt = 0;
+  // count: number | any;
+
+  // startGame() {
+  //   this.cnt = this.cnt + 1;
+  //   // console.log('Count is ' + this.cnt);
+  // }
+
+  // setIntervl(): any{
+  //   setInterval(() => this.startGame(), 1000);
+  //   this.gameStarted.emit({
+  //     count: this.cnt
+  //   });
+  // }
+
+  // stopIntervl(){
+  //   clearInterval(this.setIntervl());
+  // }
 
 
 
