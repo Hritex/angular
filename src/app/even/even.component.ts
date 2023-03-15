200604597ca4240d359dcedda0e039144713f244
hritex <neon147369@gmail.com>
2023-03-15 10:36:41 +0530

    pause3

--- src/app/even/even.component.ts
+++ src/app/even/even.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./even.component.css']
 })
 export class EvenComponent {
+  ref: number | any;
+
+  onGameStarted(eventData: any){
+    console.log(eventData.target.innerHTML);
+    this.ref = eventData.target.innerHTML;
+  }
 
 }
